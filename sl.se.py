f8ca91ac9c6df8b196e7546ed0ed09c5c1bc8e35
itgsod-daniel-krona <32324630+itgsod-daniel-krona@users.noreply.github.com>
2017-10-25 13:07:59 +0200

    Add files via upload

--- sl.se.py
+++ sl.se.py
@@ -1,11 +1,36 @@
-def fares(age, student=False, senior=False):
-    print age,student,senior
-    return 'hel'
+def fares(age, child=False, student=False, senior=False):
+    if child or student or senior:
+        return 'Halv'
+    else:
+        if age < 18 or age > 65:
+            return 'Halv'
+        else:
+            return 'Hel'
 
 
+# tests
+print 'child, 7', fares(7)
+print 'child, 6', fares(6, child=True)
+print 'young adult, 17', fares(17)
+print 'student, 16', fares(16, student=True)
+print 'adult, 23', fares(23)
+print 'student, 23', fares(23, student=True)
+print 'adult, 59', fares(59)
+print 'senior, 59', fares(59, senior=True)
+print 'senior, 67', fares(67)
 
 
+#ett till program som ocksa funkar
+#age = 'Ticket price'
 
-fares(10)
-fares(65,senior=True)
-fares(25,student=True)
+#for age in range(0, 151):
+    #print age
+    #if age > 65 < 151:
+        #print 'Price = Half a ticket'
+    #elif age > 18 < 64:
+        #print 'Price = Full ticket'
+    #elif age > 7 < 17:
+        #print 'Price = Half a ticket'
+    #elif age > 1 < 6:
+        #print 'Price = Free'+
+        #charlotte callaa
